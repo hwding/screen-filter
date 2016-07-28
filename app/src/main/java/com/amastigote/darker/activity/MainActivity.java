ee859fb203d0a473c15bbb68e8b607eefdedd695
hwding <m@jellygo.com>
2016-07-28 22:46:11 +0800

    update codes

--- app/src/main/java/com/amastigote/darker/activity/MainActivity.java
+++ app/src/main/java/com/amastigote/darker/activity/MainActivity.java
@@ -149,28 +149,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
             startActivity(new Intent(Intent.ACTION_VIEW,
                     Uri.parse("https://github.com/hwding/make-it-darker")));
-        }
-
-        else if (id == R.id.action_licenses) {
+        else if (id == R.id.action_licenses)
             startActivity(new Intent(MainActivity.this, LicenseActivity.class));
-        }
-
         else if (id == R.id.keep_screen_on_toggle) {
             if(item.isChecked())
                 currentDarkerSettings.setKeepScreenOn(false);
@@ -182,7 +172,6 @@ public class MainActivity extends AppCompatActivity {
                 currentDarkerSettings.saveCurrentSettings();
             Snackbar.make(view, "偏好配置已保存", Snackbar.LENGTH_LONG).show();
         }
-
         return super.onOptionsItemSelected(item);
     }
 
