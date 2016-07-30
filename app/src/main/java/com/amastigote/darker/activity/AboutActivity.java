0535c02ef7f3756b74be86e20c594fca56435d4f
hwding <m@jellygo.com>
2016-07-30 19:12:38 +0800

    update codes

--- app/src/main/java/com/amastigote/darker/activity/AboutActivity.java
+++ app/src/main/java/com/amastigote/darker/activity/AboutActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.amastigote.darker.R;
 
 public class AboutActivity extends AppCompatActivity {
